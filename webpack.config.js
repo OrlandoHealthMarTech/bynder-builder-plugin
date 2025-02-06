02e4291a104e4128033eba3a8fd634870fd65e38
John Schneider <143269324+jhs129@users.noreply.github.com>
2025-02-06 18:05:48 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+// contents of webpack.config.js
+const path = require("path");
+const pkg = require("./package.json");
+
+module.exports = {
+  entry: `./src/${pkg.entry}.tsx`,
+  externals: {
+    "@builder.io/react": "@builder.io/react",
+    "@builder.io/app-context": "@builder.io/app-context",
+    "@emotion/core": "@emotion/core",
+    react: "react",
+    "react-dom": "react-dom",
+  },
+  output: {
+    filename: pkg.output,
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "system",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    port: 1268,
+    static: {
+      directory: path.join(__dirname, "./dist"),
+    },
+    headers: {
+      "Access-Control-Allow-Private-Network": "true",
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+};
