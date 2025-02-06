7533f40334c568e060b1d3ff0e701c4446f2a316
jhs129 <john.schneider@hmhn.org>
2025-02-06 18:22:43 -0500

    working checkpoint

--- src/plugin.tsx
+++ src/plugin.tsx
@@ -35,6 +35,10 @@ function PluginExample(props: PluginExampleProps) {
 }
 
 Builder.registerEditor({
-  name: "MyPluginExample",
+  name: "DAMAssetSelector",
+  image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAM1BMVEVHcEwSbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf4Sbf5iEzacAAAAEXRSTlMATmpgJkKy/+WQDflzyzOBF9ldCioAAACgSURBVHgBtdFRDoQgDATQImgLK7L3P+2mk1kb02/nRyYvoTXImylbbdFa3fa7HOox8m6oHWV8lCleT2XMm+k/k8YcIhWHOcPQPE0mbyhhTeTy7xKfyN1oXz923Ot1JJPqiH1qMvxel84pT1uKaUOhT0Mx3O5ZybTFMUy2sNBsHB9Ww0KThbqNKxsy8D69JyMbIBmXDMspIONimdd5yfv5AWOTCg2BQ0hHAAAAAElFTkSuQmCC",
   component: PluginExample,
 });
+
+
+
